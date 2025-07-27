d728adc249253c343f7d80d7d662d43f0b340896
jnbn98 <jan.benn@mni.thm.de>
2025-07-27 14:54:51 +0200

    Initial commit: Spring Boot esportLab backend project
    - Spring Boot 3.5.4 with Java 21
    - JPA, OAuth2, Security, Quartz scheduler
    - PostgreSQL database with Flyway migrations
    - Basic project structure with tests

---
+++ src/main/java/run/esportLab/esportLab/EsportLabApplication.java
@@ -0,0 +1,13 @@
+package run.esportLab.esportLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/run/esportLab/esportLab/EsportLabApplicationTests.java
@@ -0,0 +1,13 @@
+package run.esportLab.esportLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
